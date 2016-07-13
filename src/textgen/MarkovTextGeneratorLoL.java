9cb632a4e1f1375890125e076ade09cbbf48933d
alex_jd <twoalexan@mail.ru>
2016-07-13 17:52:57 +0500

    Markov_generator_rework

--- src/textgen/MarkovTextGeneratorLoL.java
+++ src/textgen/MarkovTextGeneratorLoL.java
@@ -3,10 +3,10 @@ package textgen;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 
 
 /** 
@@ -25,7 +25,7 @@ public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
 	private Random rnGenerator;
 	
 	private ListNode tempListNode;
-	private ListNode lastListNode;
+	//private ListNode lastListNode;
 	
 	public MarkovTextGeneratorLoL(Random generator)
 	{
@@ -37,40 +37,46 @@ public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
 	
 	/** Train the generator by adding the sourceText */
 	@Override
-	public void train(String sourceText)
+	public void train(String sourceText) throws IndexOutOfBoundsException
 	{
 		// TODO: Implement this method
-		LLNode prevWord = null;
-		LLNode w = null;
-		MyLinkedList<String> tokens = getTokens("[a-zA-Z]+", sourceText);
-		starter = tokens.head.next.data;
-		System.out.println(starter.toString());
-		prevWord = tokens.head.next;
-		for (int i = 0; i <= tokens.size(); i++) {
-			w = prevWord.next;
-			if (w.data != null) {
-				if ( isNode( prevWord.data.toString() ) ) {
-					tempListNode.addNextWord(w.data.toString());
+		String prevWord = null;
+		String w = null;
+		List<String> tokens = getTokens("[a-zA-Z]+", sourceText);
+		try {
+			//System.out.println(tokens);
+			starter = tokens.get(0);
+			//System.out.println(starter.toString());
+			prevWord = starter;
+			for (int i = 0; i < (tokens.size() - 1); i++) {
+				w = tokens.get(i + 1);
+				if ( isNode( prevWord.toString() ) ) {
+					tempListNode.addNextWord(w.toString());
 					//System.out.println("train isNode true\n");
 				}
 				else {
 					//System.out.println("Create new ListNode " + prevWord.data.toString());
-					tempListNode = new ListNode(prevWord.data.toString());
+					tempListNode = new ListNode(prevWord.toString());
 					wordList.add(tempListNode);
-					tempListNode.addNextWord(w.data.toString());
+					tempListNode.addNextWord(w.toString());
 					//System.out.println("train isNode FALSE\n");
 				}
 				prevWord = w;
 			}
-			else {
-				if ( !isNode( prevWord.data.toString() ) ) {
-					tempListNode = new ListNode(prevWord.data.toString());
+				if ( isNode( prevWord.toString() ) ) {
+					tempListNode.addNextWord(starter);
+				}
+				else {
+					tempListNode = new ListNode(prevWord.toString());
 					wordList.add(tempListNode);
 					tempListNode.addNextWord(starter);
 				}
-			}
+				
+		}
+		catch (IndexOutOfBoundsException e) {
 			
 		}
+		
 	}
 	
 	/** 
@@ -79,7 +85,27 @@ public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
 	@Override
 	public String generateText(int numWords) {
 	    // TODO: Implement this method
-		return null;
+		String currWord = starter;
+		String output = "";
+		String w = "";
+		output = output + currWord + " ";
+		for (int i = 1; i < numWords; i++) {
+			tempListNode = nodeCorresp(currWord);
+			try {
+				w = tempListNode.getRandomNextWord(rnGenerator);
+				output += w + " ";
+				currWord = w;
+				//numWords --;
+			}
+			catch (NullPointerException e) {
+				System.out.println("Error during runtime: " + e);
+				//return null;
+			}
+			
+		}
+		
+		
+		return output;
 	}
 	
 	
@@ -101,16 +127,32 @@ public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
 	{
 		// TODO: Implement this method.
 		wordList = new LinkedList<ListNode>();
-		tempListNode = null;
-		starter = null;
+		//tempListNode = null;
+		//starter = null;
 		train(sourceText);
 		
 	}
 	
 	// TODO: Add any private helper methods you need here.
-	protected MyLinkedList<String> getTokens(String pattern, String text)
+	protected ListNode nodeCorresp (String currWord) {
+		wordList = new LinkedList<ListNode>();
+		try {
+			for (ListNode n : wordList) {
+				//System.out.println(n);
+				if ( n.getWord().equals(currWord) ){
+					return n;
+				}
+			}
+		}
+		catch (NullPointerException e) {
+			return null;
+		}
+		return null;
+	}
+	
+	protected List<String> getTokens(String pattern, String text)
 	{
-		MyLinkedList<String> tokens = new MyLinkedList<String>();
+		ArrayList<String> tokens = new ArrayList<String>();
 		Pattern tokSplitter = Pattern.compile(pattern);
 		Matcher m = tokSplitter.matcher(text);
 		
@@ -120,6 +162,7 @@ public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
 		
 		return tokens;
 	}
+
 	
 	protected boolean isNode(String word) {
 		for (ListNode n : wordList) {
@@ -177,6 +220,18 @@ public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
 		gen.retrain(textString2);
 		System.out.println(gen);
 		System.out.println(gen.generateText(20));
+		
+		String textEmpty = "";
+		System.out.println(textEmpty);
+		gen.retrain(textEmpty);
+		System.out.println(gen);
+		System.out.println(gen.generateText(10));
+		
+		System.out.println(textString2);
+		gen.retrain(textString2);
+		System.out.println(gen);
+		System.out.println(gen.generateText(20));
+		
 	}
 
 }
@@ -212,7 +267,21 @@ class ListNode
 		// TODO: Implement this method
 	    // The random number generator should be passed from 
 	    // the MarkovTextGeneratorLoL class
-	    return null;
+		//Random index = generator;
+		//System.out.println(word);
+		//System.out.println(nextWords);
+		try {
+			String randomWord = nextWords.get( generator.nextInt(getListSize()) );
+			return randomWord;
+		}
+		catch (NullPointerException e) {
+			return "The nextWord list is empty";
+		}
+		//System.out.println("randomWord " + randomWord);
+	}
+	
+	public int getListSize() {
+		return this.nextWords.size();
 	}
 
 	public String toString()
